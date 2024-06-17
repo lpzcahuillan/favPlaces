1be8fbc3160c9a6b11869e443b874cb754618a19
Hugo Luis Lopez Cahuillan <61170631+lpzcahuillan@users.noreply.github.com>
2024-06-16 22:56:44 -0400

    first commt

---
+++ FavPlacesApp/FavPlacesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavPlacesApp
+//
+//  Created by Hugo López on 16-06-24.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Map()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FavPlacesApp/FavPlacesApp/FavPlacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavPlacesAppApp.swift
+//  FavPlacesApp
+//
+//  Created by Hugo López on 16-06-24.
+//
+
+import SwiftUI
+
+@main
+struct FavPlacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
