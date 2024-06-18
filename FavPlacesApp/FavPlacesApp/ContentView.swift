a0f094c314b4a0b886759a02721ee61eb17a91de
Hugo Luis Lopez Cahuillan <61170631+lpzcahuillan@users.noreply.github.com>
2024-06-18 17:29:54 -0400

    Update ContentView.swift

--- FavPlacesApp/FavPlacesApp/ContentView.swift
+++ FavPlacesApp/FavPlacesApp/ContentView.swift
@@ -9,10 +9,32 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State var isPresented: Bool = false
     var body: some View {
-        VStack {
+        ZStack {
             Map()
+            VStack{
+                Spacer()
+                VStack{
+                    Button(action: {isPresented = true}, label: {
+                        Text("Lugares Favoritos")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, maxHeight: 80)
+                            .background(.white)
+                            .cornerRadius(16)
+                            .padding()
+                    })
+                }
+            }
+        }.sheet(isPresented: $isPresented, onDismiss: { isPresented = false}) {
+            ZStack{
+                Text("hola")
+            }
         }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .blur(radius: 5)
     }
 }
 
